3a8d306578d877cdd0a8de521df1d744b1eee1e5
DeniseMelo <melodenise.ti@gmail.com>
2022-08-29 22:03:12 -0300

    criando card

---
+++ assets/js/scripts.js
@@ -0,0 +1,71 @@
+let div = document.getElementById('cards')
+
+
+
+let divFile = document.createElement('div')
+divFile.classList.add('cardFile')
+div.appendChild(divFile)
+
+
+let img = document.createElement('img')
+img.classList.add('preview')
+divFile.appendChild(img)
+
+
+function readImage() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+          img.src= e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+
+ document.getElementById("img-input").addEventListener("change", readImage, false);
+
+
+ 
+function clique(e){
+    
+    let nome = document.createElement("h4")
+    nome.innerHTML = `${inputNome.value}`
+    divFile.appendChild(nome)
+
+    let email = document.createElement("h5")
+    email.innerHTML =`${inputEmail.value}`
+    divFile.appendChild(email)
+
+
+    let cargo = document.createElement("h5")
+    cargo.innerHTML = `${inputCargo.value}`
+    divFile.appendChild(cargo)
+
+    let local = document.createElement("h5")
+    local.innerHTML = `${inputLocal.value}`
+    divFile.appendChild(local)
+
+    let data = document.createElement("h5")
+    data.innerHTML = `${inputDate.value}`
+    divFile.appendChild(data)
+
+    let btn1 = document.createElement("button")
+    divFile.appendChild(btn1)
+    btn1.classList.add('btnCadastrar')
+    btn1.innerHTML= 'Cadastrar'
+
+    let btn2 = document.createElement("button")
+    btn2.innerHTML= 'Cancelar'
+    btn2.classList.add('btnCancelar')
+    divFile.appendChild(btn2)
+
+    divFile.classList.toggle('cardFuncionario')
+
+   if (typeof e !== 'undefined') {
+        e.preventDefault()
+    }
+    
+} 
+
+
+
